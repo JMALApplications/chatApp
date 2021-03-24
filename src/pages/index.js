b8b9c1c2c5f2a5000f58f222af44c2cc22b9226a
Jim <jmerk12086@gmail.com>
2021-03-24 19:49:46 -0400

    feat(header.js,index.js) Added base site header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Header from "../components/header/header";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+
+
+    <Header />
+
+    
+  );
 }
