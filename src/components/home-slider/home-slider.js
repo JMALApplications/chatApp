358922139105945607ecfbadf2e978eb7a43fae4
Jim <jmerk12086@gmail.com>
2021-03-25 18:44:54 -0400

    feat(home-slider.js) updated HomeSlider component

--- src/components/home-slider/home-slider.js
+++ src/components/home-slider/home-slider.js
@@ -4,15 +4,14 @@ import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 
 const slides = [
-    <StaticImage src="../../assets/images/purple_bg_1.jpg" alt="Purple Background" />,
-    <StaticImage src="../../assets/images/tech_1.jpg" alt="Purple Tech Background" />
+    <StaticImage src="../../assets/images/purple_bg_1.jpg" alt="Purple Background" style={{height: '500px'}} />,
 ];
 
 const HomeSlider = () => {
 
     return (
 
-        <AliceCarousel items={slides} />
+        <AliceCarousel autoPlay disableButtonsControls disableDotsControls items={slides} />
 
     );
 
