438eb0f4ca9170282816006f9306762388369257
Jim <jmerk12086@gmail.com>
2021-03-25 19:08:36 -0400

    feat(header.js) added dynamic site title

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-const Header = () => (
+const Header = ({siteTitle}) => (
 
     <header className="sticky top-0 bg-purple-900 text-white">
 
         <nav className="block p-5">
 
-            <Link to="/" className="float-left pr-10">Site Title</Link>
+            <Link to="/" className="float-left pr-10">{siteTitle}</Link>
             <Link to="/about">About</Link>
 
         </nav>
