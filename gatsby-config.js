308ca7c0c555e3fdcbc33ee586de6c08106c71d1
Jim <jmerk12086@gmail.com>
2021-03-25 18:00:37 -0400

    chore(gatsby-config.js) added gatsby-source-filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,20 @@ module.exports = {
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+      },
+    },
   ],
 }
