315e736a98411edcec00c271255d0687140c9679
Jim <jmerk12086@gmail.com>
2021-03-25 18:15:48 -0400

    feat(home-slider.js) updated home slider

--- src/components/home-slider/home-slider.js
+++ src/components/home-slider/home-slider.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
-import image1 from '.../assets/images/purple_bg_1.jpg';
-import image2 from 'src/assets/images/tech_1.jpg';
 
 const slides = [
-    <StaticImage src="../assets/images/purple_bg_1.jpg" alt="Purple Background" />,
+    <StaticImage src="../../assets/images/purple_bg_1.jpg" alt="Purple Background" />,
+    <StaticImage src="../../assets/images/tech_1.jpg" alt="Purple Tech Background" />
 ];
 
 const HomeSlider = () => {
