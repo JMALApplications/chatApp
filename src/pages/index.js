7a85700a24117f9e237fc12d762dd49270e8fa37
Jim <jmerk12086@gmail.com>
2021-03-25 19:09:21 -0400

    feat(index.js) added dynamic site title, graphql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,27 @@
-import React from "react"
+import React from "react";
+import { graphql } from "gatsby";
 import Header from "../components/header/header";
 import HomeSlider from "../components/home-slider/home-slider";
 
-export default function Home() {
+export default function Home({data}) {
   return (
 
     <div>
 
-      <Header />
+      <Header siteTitle={data.site.siteMetadata.title} />
       <HomeSlider />
 
     </div>
     
   );
 }
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
