8f7848cf2556f223fd7ef12fc50d6c72c4604b5b
Jim <jmerk12086@gmail.com>
2021-03-25 15:55:31 -0400

    style(header.js) updated header tailwind css classes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,9 +3,9 @@ import { Link } from 'gatsby';
 
 const Header = () => (
 
-    <header className="top-0 .sticky bg-purple-900 text-white">
+    <header className="sticky top-0 bg-purple-900 text-white">
 
-        <nav className="inline-block p-5">
+        <nav className="block p-5">
 
             <Link to="/" className="float-left pr-10">Site Title</Link>
             <Link to="/about">About</Link>
