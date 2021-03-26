7d158a6686d082a523d3971e6e1f3d3dfe5566a7
Jim <jmerk12086@gmail.com>
2021-03-26 19:25:17 -0400

    feat(info-box-list.js) created InfoBoxList component

--- src/components/info-box/info-box-list.js
+++ src/components/info-box/info-box-list.js
@@ -3,9 +3,14 @@ import React from 'react'
 const InfoBoxList = (props) => {
 
     return (
-        <div className="container grid-cols-1">
-            <div className="grid">
-                {props.children}
+        <div className="bg-gray-100">
+            <div className="container mx-auto px-20 pt-10 pb-10">
+                <h1 className="text-center font-bold mb-5 text-lg">
+                    {props.infoBoxListHeading}
+                </h1>
+                <div className="grid grid-cols-3 gap-4 justify-items-center items-center">
+                    {props.children}
+                </div>
             </div>
         </div>
     );
