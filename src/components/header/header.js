c53a6e9b735367c6571081d98f1b0f0a26e31e41
Jim <jmerk12086@gmail.com>
2021-03-27 19:42:21 -0400

    feat(header.js) updated Header to be responsive

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,29 @@
 import React from "react"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
 
 const Header = ({ siteTitle }) => (
   <header className="sticky top-0 bg-purple-900 text-white z-50">
-    <nav className="inline-block p-5">
-      <Link to="/" className="m-auto pr-10 text-xl font-bold">
-        {siteTitle}
-      </Link>
-      <Link to="/about">About</Link>
+    <nav className="container md:inline-block pt-5 pb-5 px-8 mx-auto">
+      <div className="grid grid-cols-8 md:inline-block">
+        <div>
+          <StaticImage 
+              src="../../assets/images/menu-icon.svg" 
+              alt="menu icon"
+              className="md:hidden"
+              style={{maxWidth: "30px"}}
+            />
+        </div>
+        <div className="text-center mx-auto md:m-auto col-span-6 md:inline-block">
+          <Link to="/" className="md:text-left md:pr-10 text-xl font-bold">
+            {siteTitle}
+          </Link>
+        </div>
+        <div className="hidden text-center col-span-6 md:inline-block md:m-auto text-lg">
+          <Link to="/about">About</Link>
+        </div>
+      </div>
     </nav>
   </header>
 )
