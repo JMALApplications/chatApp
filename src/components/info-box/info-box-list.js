faf4ac777842d99a2d8ff840feb0ec0eb4a433da
Jim <jmerk12086@gmail.com>
2021-03-27 17:15:25 -0400

    feat(info-box-list.js) updated InfoBoxList to be responsive

--- src/components/info-box/info-box-list.js
+++ src/components/info-box/info-box-list.js
@@ -4,11 +4,11 @@ const InfoBoxList = (props) => {
 
     return (
         <div className="bg-gray-100">
-            <div className="container mx-auto px-20 pt-10 pb-10">
+            <div className="container mx-auto px-10 md:px-20 pt-10 pb-10">
                 <h1 className="text-center font-bold mb-5 text-lg">
                     {props.infoBoxListHeading}
                 </h1>
-                <div className="grid grid-cols-3 gap-4 justify-items-center items-center">
+                <div className="grid xl:grid-cols-3 md:grid-cols-3 gap-4 justify-items-center items-center">
                     {props.children}
                 </div>
             </div>
