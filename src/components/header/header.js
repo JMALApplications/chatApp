93f65b92de38346acbaed719d071ac9ec10f0ae4
Jim <jmerk12086@gmail.com>
2021-03-27 18:28:37 -0400

    refactor(header.js) changed header css styling

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,8 +3,8 @@ import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
   <header className="sticky top-0 bg-purple-900 text-white z-50">
-    <nav className="block p-5">
-      <Link to="/" className="float-left pr-10">
+    <nav className="inline-block p-5">
+      <Link to="/" className="m-auto pr-10 text-xl font-bold">
         {siteTitle}
       </Link>
       <Link to="/about">About</Link>
