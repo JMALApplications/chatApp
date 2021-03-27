58a0734d7b05bd972f134585a1dbe7f91d805ca8
Jim <jmerk12086@gmail.com>
2021-03-27 18:26:31 -0400

    feat(index.js) added PageHeading component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,18 @@ import HomeSlider from "../components/home-slider/home-slider"
 import Layout from "../components/layout/layout"
 import InfoBoxList from "../components/info-box/info-box-list"
 import InfoBox from "../components/info-box/info-box"
+import PageHeading from "../components/page-heading/page-heading"
 import LoremIpsum from 'react-lorem-ipsum'
 
 export default function Home() {
   return (
     <Layout>
       <HomeSlider />
+      <PageHeading
+        heading="JMAL Chat App"
+        desc="A customer service chat application built with the latest technology"
+        mobileOnly={true}
+      />
       <InfoBoxList infoBoxListHeading="What is the JMAL Chat App?">
         <InfoBox 
           infoBoxHeading="Chat With Site Visitors"
