c314d160cfa3949bf200f61ad71cadfc18a04e09
Jim <jmerk12086@gmail.com>
2021-04-06 17:57:41 -0400

    feat(conversations.js) added conversations components

--- src/pages/admin/conversations.js
+++ src/pages/admin/conversations.js
@@ -1,12 +1,28 @@
 import React from "react"
+import ConversationsContainer from "../../components/admin/conversations/conversations-container"
+import ConversationsItem from "../../components/admin/conversations/conversations-item"
+import ConversationsList from "../../components/admin/conversations/conversations-list"
 
 const Conversations = () => {
 
     return (
 
-        <div>
-            
-        </div>
+        <ConversationsContainer>
+            <ConversationsList>
+                <ConversationsItem
+                    conversationUrl="/admin/conversations/user-1"
+                    visitorId="72512"
+                    conversationDate="April 6"
+                    conversationExcerpt="Hi this is a test message of a test"
+                />
+                <ConversationsItem
+                    conversationUrl="/admin/conversations/user-2"
+                    visitorId="72513"
+                    conversationDate="April 4"
+                    conversationExcerpt="Hi this is another test message of a test"
+                />
+            </ConversationsList>
+        </ConversationsContainer>
 
     )
 
