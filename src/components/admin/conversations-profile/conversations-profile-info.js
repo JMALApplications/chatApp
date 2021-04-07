8a252504d0f25693450d41a42117f9ed295b5a56
Jim <jmerk12086@gmail.com>
2021-04-07 19:01:36 -0400

    feat(conversations-profile-info.js) added component ConversationsProfileInfo

---
+++ src/components/admin/conversations-profile/conversations-profile-info.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { BsFillPersonFill } from "@react-icons/all-files/Bs/BsFillPersonFill"
+import { AiOutlineMail } from "@react-icons/all-files/Ai/AiOutlineMail"
+import { FiPhone } from "@react-icons/all-files/Fi/FiPhone"
+import { RiComputerLine } from "@react-icons/all-files/Ri/RiComputerLine"
+import { GoLocation } from "@react-icons/all-files/Go/GoLocation"
+
+const ConversationsProfileInfo = (props) => {
+
+    return (
+
+        <div className="bg-white w-full h-64 mt-5 px-5 py-2 rounded-md shadow-md">
+            <h2 className="font-bold mb-4">Visitor Info</h2>
+            <div ClassName="grid grid-rows-5 grid-flow-col gap-4 mt-5">
+                <div className="flex">
+                    <BsFillPersonFill className="text-indigo-500 text-xl mr-3"/>
+                    <input type="text" className="w-full px-1 border-b-2 border-indigo-500" placeholder="Visitor Name..." />
+                    {/* {props.visitorName} */}
+                </div>
+                <div className="flex mt-2">
+                    <AiOutlineMail className="text-indigo-500 text-xl mr-3"/>
+                    <input type="text" className="w-full px-1 border-b-2 border-indigo-500" placeholder="Visitor Email..." />
+                    {/* {props.visitorEmail} */}
+                </div>
+                <div className="flex mt-2">
+                    <FiPhone className="text-indigo-500 text-xl mr-3"/>
+                    <input type="text" className="w-full px-1 border-b-2 border-indigo-500" placeholder="Visitor Phone..." />
+                    {/* {props.visitorPhoneNumber} */}
+                </div>
+                <div className="flex mt-2">
+                    <RiComputerLine className="text-indigo-500 text-xl mr-3"/>
+                    {props.visitorOS}
+                </div>
+                <div className="flex mt-2">
+                    <GoLocation className="text-indigo-500 text-xl mr-3"/>
+                    {props.visitorIP}
+                </div>
+            </div>
+        </div>
+        
+    )
+
+}
+
+export default ConversationsProfileInfo
