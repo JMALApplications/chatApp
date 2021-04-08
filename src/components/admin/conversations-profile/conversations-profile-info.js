f86334c17c311400471ebc048282c155a1a8f5f8
Jim <jmerk12086@gmail.com>
2021-04-08 16:25:24 -0400

    style(conversations-profile-info.js) updated grid styling

--- src/components/admin/conversations-profile/conversations-profile-info.js
+++ src/components/admin/conversations-profile/conversations-profile-info.js
@@ -9,29 +9,29 @@ const ConversationsProfileInfo = (props) => {
 
     return (
 
-        <div className="bg-white w-full h-64 mt-5 px-5 py-2 rounded-md shadow-md">
+        <div className="bg-white w-full mt-5 px-5 py-2 rounded-md shadow-md">
             <h2 className="font-bold mb-4">Visitor Info</h2>
-            <div ClassName="grid grid-rows-5 grid-flow-col gap-4 mt-5">
+            <div className="grid grid-rows-5 grid-flow-col gap-4 mt-5 flex">
                 <div className="flex">
                     <BsFillPersonFill className="text-indigo-500 text-xl mr-3"/>
                     <input type="text" className="w-full px-1 border-b-2 border-indigo-500" placeholder="Visitor Name..." />
                     {/* {props.visitorName} */}
                 </div>
-                <div className="flex mt-2">
+                <div className="flex mt-1">
                     <AiOutlineMail className="text-indigo-500 text-xl mr-3"/>
                     <input type="text" className="w-full px-1 border-b-2 border-indigo-500" placeholder="Visitor Email..." />
                     {/* {props.visitorEmail} */}
                 </div>
-                <div className="flex mt-2">
+                <div className="flex mt-1">
                     <FiPhone className="text-indigo-500 text-xl mr-3"/>
                     <input type="text" className="w-full px-1 border-b-2 border-indigo-500" placeholder="Visitor Phone..." />
                     {/* {props.visitorPhoneNumber} */}
                 </div>
-                <div className="flex mt-2">
+                <div className="flex mt-1">
                     <RiComputerLine className="text-indigo-500 text-xl mr-3"/>
                     {props.visitorOS}
                 </div>
-                <div className="flex mt-2">
+                <div className="flex mt-1">
                     <GoLocation className="text-indigo-500 text-xl mr-3"/>
                     {props.visitorIP}
                 </div>
