b9d6eb5c7f2ed4f12620319bd073422e9f7539c2
Jim <jmerk12086@gmail.com>
2021-04-08 16:26:02 -0400

    style(chat-container.js) updated container styling

--- src/components/admin/chat/chat-container.js
+++ src/components/admin/chat/chat-container.js
@@ -4,7 +4,7 @@ const ChatContainer = (props) => {
 
     return (
 
-        <div className="w-7/12 h-screen bg-gray-100">
+        <div className="w-7/12 h-screen bg-gray-100 flex flex-col relative">
             {props.children}
         </div>
 
