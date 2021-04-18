e3f7807980e8b3ff3f3b1c75a5b2f6e3fbbf8a40
Anderson <laventurepsu1@gmail.com>
2021-04-18 19:41:33 -0400

    added sidePanel to conversations.js

--- src/pages/admin/visitors.js
+++ src/pages/admin/visitors.js
@@ -1,7 +1,7 @@
 import React from "react"
-import { ChatIcon, UsersIcon } from "@heroicons/react/outline"
-import { Link } from "gatsby"
+import { UsersIcon } from "@heroicons/react/outline"
 import { StaticImage } from "gatsby-plugin-image"
+import SidePanel from "../../components/admin/side-panel/side-panel"
 
 const Avator = (
   <StaticImage
@@ -36,34 +36,7 @@ const Visitors = () => {
   return (
     <>
       <div className="h-screen flex overflow-hidden bg-gray-100">
-        <div className="hidden md:flex md:flex-shrink-0 w-16">
-          <div className="flex flex-col w-64">
-            <div className="flex flex-col h-0 flex-1">
-              <div className="flex-1 flex flex-col overflow-y-auto w-16">
-                <nav className="flex-1 px-2 py-4 bg-gray-800 space-y-1">
-                  <a className="flex-col text-white text-gray-300 flex items-center px-2 py-2 text-sm font-medium rounded-md">
-                    <div>
-                      <Link to="/admin/conversations">
-                        <ChatIcon
-                          className="text-gray-400 hover:text-gray-300 mb-9 mr-3 h-6 w-6"
-                          aria-hidden="true"
-                        />
-                      </Link>
-                    </div>
-                    <div>
-                      <Link to="/admin/visitors">
-                        <UsersIcon
-                          className="text-gray-400 hover:text-gray-300 mr-3 h-6 w-6"
-                          aria-hidden="true"
-                        />
-                      </Link>
-                    </div>
-                  </a>
-                </nav>
-              </div>
-            </div>
-          </div>
-        </div>
+        <SidePanel />
         <div className="flex flex-col w-0 flex-1 overflow-hidden">
           <div className="relative z-10 flex-shrink-0 flex h-16 bg-white shadow">
             <div className="flex-1 px-4 flex ">
